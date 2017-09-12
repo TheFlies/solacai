619aa9c96e6737a93539c84a558e3ccca4bbc96b
gaconkzk <gaconkzk@gmail.com>
2017-09-12 21:23:19 +0700

    ??? eh

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+var dotenv = require('dotenv');
+// There's no need to check if .env exists, dotenv will check this // for you. It will show a small warning which can be disabled when // using this in production.
+dotenv.load();
+
 var restify = require('restify');
 var builder = require('botbuilder');
 
