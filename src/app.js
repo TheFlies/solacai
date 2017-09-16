ee7435d304e1688339189341d3cd75b120ae5544
gaconkzk <gaconkzk@gmail.com>
2017-09-16 12:33:31 +0700

    a little clean

--- src/app.js
+++ src/app.js
@@ -126,7 +126,7 @@ bot.on('contactRelationUpdate', function (message) {
 
 // ------------ Bot default handler
 bot.dialog('default', function (session) {
-  let msg = session.message.text; //.toLocaleLowerCase().replace("@ruồi sờ là cai", "").trim()
+  let msg = session.message.text;
   msg = removeBotInformation(session.message.address.bot, msg);
   router.handle(session, msg);
 });
