1db244f211298d535e5e8a8d6a5354219e2e0ad7
gaconkzk <gaconkzk@gmail.com>
2019-09-04 17:11:13 +0700

    just echo

--- src/slack_bot.js
+++ src/slack_bot.js
@@ -1,21 +1,37 @@
+const { WebClient } = require('@slack/web-api')
+
+// let atok = process.env.SLACK_AUTH_TOKEN
+let btok = process.env.SLACK_BOT_USER_AUTH_TOKEN
+const web = new WebClient(btok)
+
 const supportTypes = [
   'url_verification',
   'app_mention'
 ]
 
 const listen = () => {
-  console.log('init slack bot he')
-  let atok = process.env.SLACK_AUTH_TOKEN
-  let btok = process.env.SLACK_BOT_USER_AUTH_TOKEN
+  let verify = process.env.SLACK_VERIFICATION_TOKEN
   return (req, res, next) => {
-    if (req.body && supportTypes.indexOf(req.body.type) >= 0) {
-      switch (req.body.type) {
+    console.log(btok)
+    if (req.body && verify === req.body.token) {
+      switch (req.body.event.type) {
         case supportTypes[0]:
           res.send(req.body.challenge)
           break
         case supportTypes[1]:
-          res.send('hello')
+          res.send(200)
+
+          let payload = req.body
+          if (payload.event.type === 'app_mention' && 'bot_message' !== payload.event.subtype) {
+            if (payload.event.text.includes('test')) {
+              web.chat.postMessage({ channel: payload.event.channel, text: ' echo!' })
+                .then(() => console.log('sent'))
+                .catch(err => console.error(err))
+            }
+          }
+          break
         default:
+          res.send(200)
           break
       }
 
